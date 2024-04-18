374562f2eb8986b5c14f76ed674737ab7c4dc867
EchoModernNew <167400643+EchoModernNew@users.noreply.github.com>
2024-04-18 12:02:56 +0500

    Add files via upload

---
+++ DestroyAndCri.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAndCri : MonoBehaviour
+{
+    public float delay = 5;
+    void Update()
+    {
+        print("Crikets");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Timer());
+
+        
+    }
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    
+}
---
+++ NPC.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    public int Heath = 5;
+    public int Speed = 3;
+    public int Level = 1;
+   
+    void Start()
+    {
+        print(Heath += Level);
+    }
+
+    // Update is called once per frame
+    void Update()
+ {
+        Vector3 newPosition = transform.position;
+        newPosition.x -= Speed * Time.deltaTime;
+        transform.position = newPosition;
+    }
+}
